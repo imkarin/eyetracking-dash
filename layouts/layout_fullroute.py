216022525bab33aa297b6b24c935a208fddf05db
Siebe Tolsma <siebetolsma@hotmail.nl>
2021-06-06 14:45:24 +0200

    Full route t/m head movement

--- layouts/layout_fullroute.py
+++ layouts/layout_fullroute.py
@@ -41,23 +41,65 @@ def tab_eyes(df):
                              size_max=10,
                              opacity=0.5)
 
-    fig_pupil = px.scatter(df,
+    fig_pupilscat = px.scatter(df,
                         x='ET_PupilLeft',
                         y='ET_PupilRight',
                         title='Pupil size',
                         labels={
                             "ET_PupilLeft": "Pupil left (mm)",
                             "ET_PupilRight": "Pupil right (mm)"})
+    
+    fig_pupilsbar = px.bar(df,
+                        x='ET_PupilLeft',
+                        title='Pupil size',
+                        labels={
+                            "ET_PupilLeft": "Pupil left (mm)",})
                     
     fig_2dgazeinter = px.scatter(df,
                             x='Interpolated Gaze X',
                             y='Interpolated Gaze Y',
                             title='Interpolated Gaze')
 
-    fig_2dgazeinterline = px.line(df,
-                            x='Interpolated Gaze X',
-                            y='Interpolated Gaze Y',
-                            title='Interpolated Gaze')
+    fig_gazevelacc = px.scatter(df,
+                            x='Gaze Velocity',
+                            y='Gaze Acceleration',
+                            title='Gaze Velocity and Acceleration')
+
+    fig_fixationxy = px.scatter(df,
+                            x='Fixation X',
+                            y='Fixation Y',
+                            title='Fixation X and Y')
+
+    fig_fixationstartend = px.scatter(df,
+                            x='Fixation Start',
+                            y='Fixation End',
+                            title='Fixation Start and End')
+
+    fig_velacc = px.scatter(df,
+                            x='Saccade Peak Velocity',
+                            y='Saccade Peak Acceleration',
+                            title='Peak Velocity & Peak Acceleration')
+
+    fig_accdel = px.scatter(df,
+                            x='Saccade Peak Deceleration',
+                            y='Saccade Peak Acceleration',
+                            title='Peak Deceleration & Peak Acceleration')
+
+    fig_durdir = px.scatter(df,
+                            x='Saccade Direction',
+                            y='Saccade Duration',
+                            title='Duration & Direction')
+                    
+    fig_ampvel = px.scatter(df,
+                            x='Saccade Amplitude',
+                            y='Saccade Peak Velocity',
+                            title='Amplitude & Peak Velocity')
+
+    fig_blink = px.histogram(df,
+                        x='Blink detected (binary)',
+                        title='Detected blinks',
+                        nbins=2)
+
 
     # the layout
     tab_layout = [
@@ -81,17 +123,11 @@ def tab_eyes(df):
                             width=6,
                             children=
                             [
-                                dcc.Graph(figure=fig_pupil)
+                                dcc.Graph(figure=fig_pupilscat)
                             ]
                         ),
                     ]
                 ),
-            ]
-        ),
-        html.Section(
-            className='mt-5',
-            children=
-            [
                 dbc.Row(
                     children=
                     [
@@ -102,14 +138,15 @@ def tab_eyes(df):
                                 dcc.Graph(figure=fig_2dgazeinter)
                             ]
                         ),
+                        dbc.Col(
+                            width=6,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_gazevelacc)
+                            ]
+                        ),
                     ]
                 ),
-            ]
-        ),
-        html.Section(
-            className='mt-5',
-            children=
-            [
                 dbc.Row(
                     children=
                     [
@@ -117,27 +154,301 @@ def tab_eyes(df):
                             width=6,
                             children=
                             [
-                                dcc.Graph(figure=fig_2dgazeinterline)
+                                dcc.Graph(figure=fig_fixationxy)
+                            ]
+                        ),
+                        dbc.Col(
+                            width=6,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_fixationstartend)
+                            ]
+                        ),
+                    ]
+                ),
+
+            ]
+        ),
+        html.Section(
+            className='mt-5',
+            children=
+            [
+                html.H4('Saccade'),
+                html.P(f"""- Duration (hoe lang duurt de beweging van het oog tussen de fixation points)
+                    - Amplitude (hoekafstand die de ogen van het beginpunt naar het eindpunt hebben afgelegd)
+                    - Peak velocity (de maximale snelheid van de ogen tijdens deze saccade)
+                    - Peak acceleration (de maximale snelheidstoename van de ogen tijdens deze saccade)
+                    - Peak deceleration (maximale afname van de snelheid van de ogen tijdens deze saccade)
+                    - Direction (richting van de saccade van het beginpunt naar het eindpunt, aangegeven als hoeken tegen de klok in: 0 graden betekent een horizontale saccade van links naar rechts, 90 graden een verticale saccade van onder naar boven)
+                """),
+                dbc.Row(
+                    children=
+                    [
+                        dbc.Col(
+                            width=6,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_velacc)
+                            ]
+                        ),
+                        dbc.Col(
+                            width=6,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_accdel)
+                            ]
+                        ),
+                    ]
+                ),
+                dbc.Row(
+                    children=
+                    [
+                        dbc.Col(
+                            width=6,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_durdir)
+                            ]
+                        ),
+                        dbc.Col(
+                            width=6,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_ampvel)
+                            ]
+                        ),
+                    ]
+                ),
+                dbc.Row(
+                    children=
+                    [
+                        dbc.Col(
+                            width=6,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_blink)
                             ]
                         ),
                     ]
                 ),
             ]
-        ),  
+        ),
     ]
     return tab_layout
 
 # Tab 2: GSR
 def tab_gsr(df):
+    fig_tonic = px.line(df,
+                y='Tonic signal (microSiemens)')
+    
+    fig_phasic = px.line(df,
+                y='Phasic signal (microSiemens)')
+
+    fig_gsrraw = px.line(df,
+                y='GSR RAW')
+
+    fig_gsrinter = px.line(df,
+                y='GSR Interpolated (microSiemens)(GSRPEAK=IsPeak)(GSRONSET=GsrOnset)(GSROFFSET=GsrOffset)')
+
+    fig_intertonic = px.scatter(df,
+                x='GSR Interpolated (microSiemens)(GSRPEAK=IsPeak)(GSRONSET=GsrOnset)(GSROFFSET=GsrOffset)',
+                y='Tonic signal (microSiemens)')
+
+    fig_interphasic = px.scatter(df,
+                x='GSR Interpolated (microSiemens)(GSRPEAK=IsPeak)(GSRONSET=GsrOnset)(GSROFFSET=GsrOffset)',
+                y='Phasic signal (microSiemens)')
+
+
     tab_layout = [
-        # html elements/plots here
+        html.Section(
+            className='mt-5',
+            children=
+            [
+                html.H4('Header'),
+                html.P(f'Information'),
+                dbc.Row(
+                    children=
+                    [
+                        dbc.Col(
+                            width=6,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_tonic)
+                            ]
+                        ),
+                        dbc.Col(
+                            width=6,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_phasic)
+                            ]
+                        ),
+                    ]
+                ),
+                dbc.Row(
+                    children=
+                    [
+                        dbc.Col(
+                            width=6,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_gsrraw)
+                            ]
+                        ),
+                        dbc.Col(
+                            width=6,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_gsrinter)
+                            ]
+                        ),
+                    ]
+                ),
+                dbc.Row(
+                    children=
+                    [
+                        dbc.Col(
+                            width=6,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_intertonic)
+                            ]
+                        ),
+                        dbc.Col(
+                            width=6,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_interphasic)
+                            ]
+                        ),
+                    ]
+                ),
+            ]
+        ),
     ]
     return tab_layout
 
 # Tab 3: Movement
 def tab_movement(df):
+    fig_gyrx = px.line(df,
+                y='ET_GyroX')
+
+    fig_gyry = px.line(df,
+                y='ET_GyroY')
+
+    fig_gyrz = px.line(df,
+                y='ET_GyroZ')
+
+    fig_accx = px.line(df,
+                y='ET_AccX')
+
+    fig_accy = px.line(df,
+                y='ET_AccY')
+
+    fig_accz = px.line(df,
+                y='ET_AccZ')
+
+
+
     tab_layout = [
-        # html elements/plots here
+        html.Section(
+            className='mt-5',
+            children=
+            [
+                html.H4('Header'),
+                html.P(f'Information'),
+                dbc.Row(
+                    children=
+                    [
+                        dbc.Col(
+                            width=4,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_gyrx)
+                            ]
+                        ),
+                        dbc.Col(
+                            width=4,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_gyry)
+                            ]
+                        ),
+                        dbc.Col(
+                            width=4,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_gyrz)
+                            ]
+                        )
+                    ]
+                ),
+                dbc.Row(
+                    children=
+                    [
+                        dbc.Col(
+                            width=4,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_accx)
+                            ]
+                        ),
+                        dbc.Col(
+                            width=4,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_accy)
+                            ]
+                        ),
+                        dbc.Col(
+                            width=4,
+                            children=
+                            [
+                                dcc.Graph(figure=fig_accz)
+                            ]
+                        )
+                    ]
+                ),
+        #         dbc.Row(
+        #             children=
+        #             [
+        #                 dbc.Col(
+        #                     width=6,
+        #                     children=
+        #                     [
+        #                         dcc.Graph(figure=fig_gsrraw)
+        #                     ]
+        #                 ),
+        #                 dbc.Col(
+        #                     width=6,
+        #                     children=
+        #                     [
+        #                         dcc.Graph(figure=fig_gsrinter)
+        #                     ]
+        #                 ),
+        #             ]
+        #         ),
+        #         dbc.Row(
+        #             children=
+        #             [
+        #                 dbc.Col(
+        #                     width=6,
+        #                     children=
+        #                     [
+        #                         dcc.Graph(figure=fig_intertonic)
+        #                     ]
+        #                 ),
+        #                 dbc.Col(
+        #                     width=6,
+        #                     children=
+        #                     [
+        #                         dcc.Graph(figure=fig_interphasic)
+        #                     ]
+        #                 ),
+        #             ]
+                # ),
+            ]
+        ),
     ]
     return tab_layout
 
